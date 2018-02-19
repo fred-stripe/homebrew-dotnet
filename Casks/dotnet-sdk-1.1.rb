157416388324f9caa67aece18b51abea031d6e07
Fred <fred@stripe.com>
2018-02-19 12:09:28 -0500

    Updated 1.1x package to 1.1.7 LTS
    Also tracked down the exact set of packages this installs so brew
    can clean them up with `brew cask uninstall`.

--- Casks/dotnet-sdk-1.1.rb
+++ Casks/dotnet-sdk-1.1.rb
@@ -1,8 +1,8 @@
 cask 'dotnet-sdk-1.1' do
-  version '1.1.6'
-  sha256 '5cc1b6dc8814b7fd30746ccfdab1a9feec8b3276f126d2e61dab84c807be7ae9'
+  version '1.1.7'
+  sha256 '190352005c0e8ccde97a109dc727eb88e2eaacf833e3f72bc8204d54ba017268'
 
-  url "https://download.microsoft.com/download/4/0/9/40979F7E-6DC8-4895-8ED5-0E7B45E74F94/dotnet-dev-osx-x64.#{version}.pkg"
+  url "https://download.microsoft.com/download/4/E/6/4E64A465-F02E-43AD-9A86-A08A223A82C3/dotnet-dev-osx-x64.#{version}.pkg"
   name '.NET Core SDK version 1.1.1'
   homepage 'https://github.com/dotnet/core/blob/master/release-notes/download-archives/1.1.6-sdk-download.md'
 
@@ -10,11 +10,11 @@ cask 'dotnet-sdk-1.1' do
 
   pkg "dotnet-dev-osx-x64.#{version}.pkg"
 
-  uninstall pkgutil: 'com.microsoft.dotnet.dev.1.1.6.component.osx.x64',
-   'com.microsoft.dotnet.hostfxr.1.0.1.component.osx.x64',
-   'com.microsoft.dotnet.hostfxr.1.1.0.component.osx.x64',
-   'com.microsoft.dotnet.sharedframework.Microsoft.NETCore.App.1.0.8.component.osx.x64',
-   'com.microsoft.dotnet.sharedframework.Microsoft.NETCore.App.1.1.5.component.osx.x64'
-
-  zap trash: '~/.nuget'
+  uninstall pkgutil:  [
+                      "com.microsoft.dotnet.dev.#{version}.component.osx.x64",
+                      'com.microsoft.dotnet.hostfxr.1.0.1.component.osx.x64',
+                      'com.microsoft.dotnet.hostfxr.1.1.0.component.osx.x64',
+                      'com.microsoft.dotnet.sharedframework.Microsoft.NETCore.App.1.0.9.component.osx.x64',
+                      'com.microsoft.dotnet.sharedframework.Microsoft.NETCore.App.1.1.6.component.osx.x64',
+                      ]
 end
